f8203c42401bc93129a98595a46cd2a5b0cfa8d5
Kyumin Kim <math4tots@gmail.com>
2015-09-25 08:35:10 -0700

    translator.py cont

--- translator.py
+++ translator.py
@@ -6,10 +6,8 @@ import sys
 
 SYMBOLS = ('.', '=', '(', ')', '[', ']', ',', ';', ':',)
 KEYWORDS = (
-  'include',
-  'class',
+  'class', 'lambda',
   'pass',
-  'method',
   'var',
   'return',
   'while', 'break', 'continue',
@@ -65,7 +63,7 @@ class Token(object):
 
 class CclError(Exception):
 
-  def __init__(self, message, origin):
+  def __init__(self, origin, message):
     super(CclError, self).__init__(message + '\n' + origin.LocationMessage())
 
 
@@ -89,7 +87,7 @@ def Lex(filespec, string):
 
   while True:
     "skips newlines iff depth > 0"
-    while i < len(s) and (s[i].isspace() and (depth or s[i] != '\n')) or s[i] == '#':
+    while i < len(s) and ((s[i].isspace() and (depth or s[i] != '\n')) or s[i] == '#'):
       if s[i] == '#':
         while i < len(s) and s[i] != '\n':
           i += 1
@@ -106,7 +104,7 @@ def Lex(filespec, string):
       tokens.append(MakeToken('Newline'))
       while True:
         j = i
-        while i < len(s) and s[i].isspace() and s[i] == '\n':
+        while i < len(s) and s[i].isspace() and s[i] != '\n':
           i += 1
         if i < len(s) and s[i] == '#':
           while i < len(s) and s[i] != '\n':
@@ -128,9 +126,9 @@ def Lex(filespec, string):
             tokens.append(MakeToken('Newline'))
             indent_stack.pop()
         else:
-          raise LexError('Invalid indent: ' + repr(indent), MakeOrigin())
+          raise LexError(MakeOrigin(), 'Invalid indent: ' + repr(indent))
 
-    elif s[i].isdigit() and s[i] == '.' and s[i+1:i+2].isdigit():
+    elif s[i].isdigit() or s[i] == '.' and s[i+1:i+2].isdigit():
       while i < len(s) and s[i].isdigit():
         i += 1
       if i < len(s) and s[i] == '.':
@@ -138,6 +136,20 @@ def Lex(filespec, string):
       while i < len(s) and s[i].isdigit():
         i += 1
       tokens.append(MakeToken('Number', float(s[j:i])))
+
+    elif s.startswith(('r"', "r'", '"', "'"), i):
+      raw = False
+      if s[i] == 'r':
+        i += 1
+        raw = True
+      quote = s[i:i+3] if s.startswith(('"""', "'''"), i) else s[i]
+      i += len(quote)
+      while not s.startswith(quote, i):
+        if i >= len(s):
+          raise LexError(MakeOrigin(), "Missing quotes for: " + quote)
+        i += 2 if not raw and s[i] == '\\' else 1
+      i += len(quote)
+      tokens.append(MakeToken('String', eval(s[j:i])))
 
     elif s.startswith(SYMBOLS, i):
       symbol = max(symbol for symbol in SYMBOLS if s.startswith(symbol, i))
@@ -148,13 +160,127 @@ def Lex(filespec, string):
       i += len(symbol)
       tokens.append(MakeToken(symbol))
 
-    elif s.startswith(('r"', "r'", '"', "'"), i):
-      tokens.append(MakeToken('String', eval(s[j:i])))
+    elif s[i].isalnum() or s[i] == '_':
+      while s[i].isalnum() or s[i] == '_':
+        i += 1
+      word = s[j:i]
+      if word in KEYWORDS:
+        tokens.append(MakeToken(word))
+      else:
+        tokens.append(MakeToken('Name', word))
 
     else:
       while i < len(s) and not s[i].isspace():
         i += 1
-      raise LexError("Unrecognized token: " + s[j:i], MakeOrigin())
+      raise LexError(MakeOrigin(), "Unrecognized token: " + s[j:i])
+
+  while indent_stack[-1] != '':
+    tokens.append(MakeToken('Dedent', None))
+    indent_stack.pop()
+
+  tokens.append(MakeToken('End'))
 
   return tokens
+
+### Lexer Tests
+
+origin = Origin('<test>', """
+hello world!
+""", 1)
+
+assert origin.Line() == 'hello world!', repr(origin.Line())
+
+assert origin.LocationMessage() == """in <test> on line 2 column 1
+hello world!
+*
+""", repr(origin.LocationMessage())
+
+tokens = Lex('<test>', """
+"hello".Print()
+""")
+
+assert (
+    tokens ==
+    [
+        Token(None, 'Newline', None),
+        Token(None, 'String', 'hello'),
+        Token(None, '.', None),
+        Token(None, 'Name', 'Print'),
+        Token(None, '(', None),
+        Token(None, ')', None),
+        Token(None, 'Newline', None),
+        Token(None, 'End', None),
+    ]
+), tokens
+
+tokens = Lex('<test>', """
+i = 0
+while i.LessThan(10)
+  i.Print()
+  i = i.Add(1)
+""")
+
+assert (
+    tokens ==
+    [
+        Token(None, 'Newline', None),
+
+        Token(None, 'Name', 'i'),
+        Token(None, '=', None),
+        Token(None, 'Number', 0.0),
+        Token(None, 'Newline', None),
+
+        Token(None, 'while', None),
+        Token(None, 'Name', 'i'),
+        Token(None, '.', None),
+        Token(None, 'Name', 'LessThan'),
+        Token(None, '(', None),
+        Token(None, 'Number', 10),
+        Token(None, ')', None),
+        Token(None, 'Newline', None),
+
+        Token(None, 'Indent', None),
+
+          Token(None, 'Newline', None),
+
+          Token(None, 'Name', 'i'),
+          Token(None, '.', None),
+          Token(None, 'Name', 'Print'),
+          Token(None, '(', None),
+          Token(None, ')', None),
+          Token(None, 'Newline', None),
+
+          Token(None, 'Name', 'i'),
+          Token(None, '=', None),
+          Token(None, 'Name', 'i'),
+          Token(None, '.', None),
+          Token(None, 'Name', 'Add'),
+          Token(None, '(', None),
+          Token(None, 'Number', 1),
+          Token(None, ')', None),
+          Token(None, 'Newline', None),
+
+        Token(None, 'Dedent', None),
+
+        Token(None, 'End', None),
+    ]
+), tokens
+
+try:
+  Lex('<test>', '!@#')
+except LexError as e:
+  assert str(e) == """Unrecognized token: !@#
+in <test> on line 1 column 1
+!@#
+*
+""", str(e)
+else:
+  assert False, "Lex('!@#', '<test>') should have raised error but didn't"
+
+### Parser
+
+class ParseError(CclError):
+  pass
+
+
 
