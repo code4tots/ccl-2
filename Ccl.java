e70948f8ea4bf6d76bd6c5eee249428e5d57963e
Kyumin Kim <math4tots@gmail.com>
2015-10-22 22:32:52 -0700

    xxx

--- Ccl.java
+++ Ccl.java
@@ -1,4 +1,10 @@
 // javac Ccl.java -Xlint
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,7 +23,7 @@ public class Ccl {
     if (node instanceof ArrayList)
       return invoke(context, (ArrayList) node);
 
-    throw err("Tried to eval node of type: " + node.getClass().toString());
+    throw err("Tried to eval node of type: " + node.getClass().getName());
   }
 
   //*** Functions for manipulating context.
@@ -43,6 +49,12 @@ public class Ccl {
   }
 
   //**** Invoke.
+
+  public static Object call(String name, Object... args) {
+    Func f = (Func) ROOT_CONTEXT.get(name);
+    return f.invoke(new ArrayList(Arrays.asList(args)));
+  }
+
   public static Object invoke(HashMap context, ArrayList items) {
     if (items.size() == 0)
       throw err("Tried to eval-invoke an empty list");
@@ -71,16 +83,24 @@ public class Ccl {
   }
 
   //*** For stuff where builtin types couldn't handle it.
-  abstract public static class Macro {
+  abstract public static class Builtin {
     public final String name;
-    public Macro(String name) { this.name = name; }
+    public Builtin(String name) { this.name = name; }
+    public void expectArgumentLength(int expected, int actual) {
+      if (expected != actual)
+        throw err(name + " expected " + Integer.toString(expected) + " arguments, but found " + Integer.toString(actual));
+    }
+  }
+  abstract public static class Macro extends Builtin {
+    public Macro(String name) { super(name); }
     abstract public Object invoke(HashMap context, ArrayList args);
+    public String toString() { return "<macro " + name + ">"; }
   }
 
-  abstract public static class Func {
-    public final String name;
-    public Func(String name) { this.name = name; }
+  abstract public static class Func extends Builtin {
+    public Func(String name) { super(name); }
     abstract public Object invoke(ArrayList args);
+    public String toString() { return "<func " + name + ">"; }
   }
 
   //*** parse
@@ -102,7 +122,7 @@ public class Ccl {
       items.add("block");
       skipSpaces();
       while (i < s.length()) {
-        items.add(parseOne())
+        items.add(parseOne());
         skipSpaces();
       }
       return items;
@@ -111,15 +131,15 @@ public class Ccl {
     public ArrayList parseMany() {
       ArrayList items = new ArrayList();
       skipSpaces();
-      while (s.get(i) != ')') {
-        items.add(parseOne())
+      while (s.charAt(i) != ')') {
+        items.add(parseOne());
         skipSpaces();
       }
       return items;
     }
 
     private Object parseOne() {
-      char c = s.get(i);
+      char c = s.charAt(i);
       if (c == '-' || c == '.' || Character.isDigit(c))
         return parseNumber();
 
@@ -134,28 +154,164 @@ public class Ccl {
 
     private Double parseNumber() {
       int j = i;
-      while (i < s.length() && s.get(i) != ')')
+      while (i < s.length() && (Character.isDigit(s.charAt(i)) ||
+                                s.charAt(i) == '.' ||
+                                s.charAt(i) == '-'))
         i++;
+      return Double.parseDouble(s.substring(j, i));
     }
 
     private ArrayList parseStringLiteral() {
       StringBuilder sb = new StringBuilder();
-
+      boolean raw = s.charAt(i) == 'r';
+      if (raw)
+        i++;
+      String quote = (s.startsWith("'''", i) || s.startsWith("\"\"\"", i)) ? s.substring(i, i+3) : s.substring(i, i+1);
+      i += quote.length();
+      while (!s.startsWith(quote, i)) {
+        if (i >= s.length())
+          throw err("You need end quotes!");
+        char c = s.charAt(i);
+        if (!raw && c == '\\') {
+          i++;
+          if (i >= s.length())
+            throw err("You tried to escape the string, but the file ended...");
+          c = s.charAt(i);
+          switch(c) {
+          case 'n': sb.append('\n');
+          default: throw err("Unrecognized escape: " + Character.toString(c));
+          }
+        }
+        else {
+          sb.append(c);
+          i++;
+        }
+      }
+      i += quote.length();
       ArrayList lit = new ArrayList();
       lit.add("quote");
       lit.add(sb.toString());
       return lit;
     }
 
+    private ArrayList parseForm() {
+      i++; // consume '('
+      ArrayList items = parseMany();
+      i++; // consume ')'
+      return items;
+    }
+
+    private String parseName() {
+      int j = i;
+      while (i < s.length() && !Character.isWhitespace(s.charAt(i)) && s.charAt(i) != ')')
+        i++;
+      return s.substring(j, i);
+    }
+
     private void skipSpaces() {
-      while (i < s.length() && (Character.isWhitespaceChar(s.get(i))))
+      while (i < s.length() && (Character.isWhitespace(s.charAt(i))))
         i++;
     }
   }
 
-  //*** main
-  public static void main(String[] args) {
+  //*** Root context
 
-    new ArrayList(Arrays.asList(1, 2, 3));
+  public static final HashMap ROOT_CONTEXT = new HashMap();
+  static {
+    ROOT_CONTEXT.put("block", new Macro("block") {
+      public Object invoke(HashMap context, ArrayList args) {
+        Object last = null;
+        for (int i = 0; i < args.size(); i++)
+          last = eval(context, args.get(i));
+        return last;
+      }
+    });
+
+    ROOT_CONTEXT.put("print", new Func("print") {
+      public Object invoke(ArrayList args) {
+        for (int i = 0; i < args.size(); i++) {
+          if (i > 0)
+            System.out.print(" ");
+          System.out.print(args.get(i));
+        }
+        System.out.println();
+        return args.size() > 0 ? args.get(0) : null;
+      }
+    });
+
+    ROOT_CONTEXT.put("quote", new Macro("quote") {
+      public Object invoke(HashMap context, ArrayList args) {
+        expectArgumentLength(1, args.size());
+        return args.get(0);
+      }
+    });
+
+    ROOT_CONTEXT.put("type-of", new Func("type-of") {
+      public Object invoke(ArrayList args) {
+        expectArgumentLength(1, args.size());
+        Object val = args.get(0);
+        if (val == null)
+          return "nil";
+        if (val instanceof Double)
+          return "num";
+        if (val instanceof String)
+          return "str";
+        if (val instanceof ArrayList)
+          return "list";
+        if (val instanceof HashMap)
+          return "dict";
+        throw err("Unrecognized type: " + val.getClass().getName());
+      }
+    });
+
+    ROOT_CONTEXT.put("add", new Func("add") {
+      public Object invoke(ArrayList args) {
+        double total = 0;
+        for (int i = 0; i < args.size(); i++) {
+          if (!(args.get(i) instanceof Double))
+            throw err("add expects only num arguments but found: " + call("type-of", args.get(i)).toString());
+          total += (Double) args.get(i);
+        }
+        return total;
+      }
+    });
+
+    ROOT_CONTEXT.put("join", new Func("join") {
+      public Object invoke(ArrayList args) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < args.size(); i++) {
+          if (!(args.get(i) instanceof String))
+            throw err("join expects only str arguments but found: " + call("type-of", args.get(i)).toString());
+          sb.append(args.get(i).toString());
+        }
+        return total;
+      }
+    });
+  }
+
+  //*** main
+
+  public static String slurpInputStream(InputStream stream) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+    StringBuilder sb = new StringBuilder();
+    String line = null;
+    while ((line = reader.readLine()) != null)
+      sb.append(line).append("\n");
+    reader.close();
+    return sb.toString();
+  }
+
+  public static String slurpFile(String path) throws IOException {
+    FileInputStream fin = new FileInputStream(new File(path));
+    String contents = slurpInputStream(fin);
+    fin.close();
+    return contents;
+  }
+
+  public static void main(String[] args) throws Exception {
+    if (args.length != 1)
+      throw err("Expected 1 argument but found: " + Arrays.asList(args).toString());
+    String code = slurpFile(args[0]);
+    eval(ROOT_CONTEXT, parse(code));
   }
 }
