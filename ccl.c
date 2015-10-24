3cad1d8463ff7f337f3a8bd50b4b20f1eab8d55a
Kyumin Kim <math4tots@gmail.com>
2015-10-24 01:36:11 -0700

    ccl.c

--- ccl.c
+++ ccl.c
@@ -1,8 +1,10 @@
 /** gcc -Wall -Werror -Wpedantic -std=c89 ccl.c && ./a.out */
 /** header */
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 #define CCL_RED 0
 #define CCL_BLACK 1
@@ -16,16 +18,18 @@
 #define CCL_STR 3
 #define CCL_LIST 4
 #define CCL_DICT 5
+#define CCL_FUNC 6
+#define CCL_LAMBDA 7
 
 typedef struct CCL_str CCL_str;
 typedef struct CCL_list CCL_list;
-typedef struct CCL_node CCL_node; /* node for dict */
-typedef struct CCL_dict CCL_dict; /* implemented as a self balancing tree */
+typedef struct CCL_dict CCL_dict;
+typedef struct CCL_lambda CCL_lambda;
 typedef struct CCL_Object CCL_Object;
 
 struct CCL_str {
-  size_t size, capacity;
-  char *buffer;
+  size_t size;
+  const char *buffer;
 };
 
 struct CCL_list {
@@ -33,15 +37,14 @@ struct CCL_list {
   CCL_Object **buffer;
 };
 
-struct CCL_node {
+struct CCL_dict {
   CCL_Object *key, *value;
-  CCL_node *children[2];
-  int color;
+  CCL_dict *children[2];
+  size_t size;
 };
 
-struct CCL_dict {
-  size_t size;
-  CCL_node *root;
+struct CCL_lambda {
+  CCL_Object *context, *node;
 };
 
 struct CCL_Object {
@@ -51,21 +54,64 @@ struct CCL_Object {
     double as_num;
     CCL_str as_str;
     CCL_list as_list;
-    CCL_dict as_dict;
+    CCL_dict *as_dict;
+    CCL_Object *(*as_func)(int, ...);
+    CCL_lambda as_lambda;
   } value;
 };
 
+extern CCL_Object *CCL_nil;
+extern CCL_Object *CCL_true;
+extern CCL_Object *CCL_false;
+
+void CCL_init(); /* must be called before any other things */
 int CCL_cmp(CCL_Object *left, CCL_Object *right);
+CCL_Object *CCL_num_new(double value);
+CCL_Object *CCL_str_new(const char *value);
+CCL_Object *CCL_list_new(int argc, ...);
+void CCL_list_add(CCL_Object *list, CCL_Object *item);
+CCL_Object *CCL_list_pop(CCL_Object *list);
+CCL_Object *CCL_dict_new(int argc, ...);
+void CCL_dict_setitem(CCL_Object *dict, CCL_Object *key, CCL_Object *value);
+void CCL_dict_delitem(CCL_Object *dict, CCL_Object *key);
+CCL_Object *CCL_dict_getitem(CCL_Object *dict, CCL_Object *key);
 
 /** static function headers */
 
-static int dict_find(CCL_dict *dict, CCL_Object *key, CCL_node ***nodeptr, CCL_node **parent, int *childtype);
-static int node_find(CCL_Object *key, CCL_node ***nodeptr, CCL_node **parent, int *childtype);
-
 /** implementation */
 
+#define CCL_MAX_INTERNED_NUM 200
+#define CCL_MIN_LIST_SIZE 10
+
+static int CCL_initialized = 0;
+
+static CCL_Object CCL_nil_object = { CCL_NIL };
+CCL_Object *CCL_nil = &CCL_nil_object;
+static CCL_Object CCL_true_object = {CCL_BOOL, {1}};
+CCL_Object *CCL_true = &CCL_true_object;
+static CCL_Object CCL_false_object = {CCL_BOOL, {0}};
+CCL_Object *CCL_false = &CCL_false_object;
+
+static CCL_Object nonnegative_nums[CCL_MAX_INTERNED_NUM];
+static CCL_Object negative_nums[CCL_MAX_INTERNED_NUM];
+
+void CCL_init() {
+  int i;
+
+  if (CCL_initialized)
+    return;
+
+  CCL_initialized = 1;
+
+  for (i = 0; i < CCL_MAX_INTERNED_NUM; i++) {
+    nonnegative_nums[i].type = CCL_NUM;
+    nonnegative_nums[i].value.as_num = i;
+    negative_nums[i].type = CCL_NUM;
+    negative_nums[i].value.as_num = -i;
+  }
+}
+
 int CCL_cmp(CCL_Object *left, CCL_Object *right) {
-  /* TODO */
   if (left == right)
     return 0;
 
@@ -74,51 +120,205 @@ int CCL_cmp(CCL_Object *left, CCL_Object *right) {
 
   switch(left->type) {
   case CCL_NIL: return 0;
-  case CCL_BOOL: return (left->value.as_bool && right->value.as_bool) || (!left->value.as_bool && !right->value.as_bool);
-  case CCL_NUM: return left->value.as_num == right->value.as_num;
+  case CCL_BOOL: return left->value.as_bool - right->value.as_bool;
+  case CCL_NUM: return left->value.as_num - right->value.as_num;
   case CCL_STR: return strcmp(left->value.as_str.buffer, right->value.as_str.buffer);
-  case CCL_LIST: /* TODO */
+  case CCL_LIST: {
+    int i, cmp, size = left->value.as_list.size < right->value.as_list.size ? left->value.as_list.size : right->value.as_list.size;
+    for (i = 0; i < size; i++) {
+      cmp = CCL_cmp(left->value.as_list.buffer[i], right->value.as_list.buffer[i]);
+      if (cmp != 0)
+        return cmp;
+    }
+    return left->value.as_list.size - right->value.as_list.size;
+  }
   case CCL_DICT: /* TODO */
-  default: return 0;
+  default:
+    fprintf(stderr, "Invalid type: %d", left->type);
+    exit(1);
   }
 }
 
-static int dict_find(CCL_dict *dict, CCL_Object *key, CCL_node ***nodeptr, CCL_node **parent, int *childtype) {
-  *nodeptr = &dict->root;
-  return node_find(key, nodeptr, parent, childtype);
+CCL_Object *CCL_num_new(double value) {
+  CCL_Object *num;
+
+  if (value == (int) value && value < CCL_MAX_INTERNED_NUM && -CCL_MAX_INTERNED_NUM < value)
+    return value < 0 ? &negative_nums[-(int) value] : &nonnegative_nums[(int) value];
+
+  num = malloc(sizeof(CCL_Object));
+  num->type = CCL_NUM;
+  num->value.as_num = value;
+  return num;
 }
 
-static int node_find(CCL_Object *key, CCL_node ***nodeptr, CCL_node **parent, int *childtype) {
-  int cmp;
+CCL_Object *CCL_str_new(const char *value) {
+  CCL_Object *str = malloc(sizeof(CCL_Object));
+  size_t size = strlen(value);
+  char *buffer = malloc(sizeof(char) * (size+1));
 
-  while (**nodeptr != NULL) {
-    cmp = CCL_cmp(key, (**nodeptr)->key);
+  strcpy(buffer, value);
+
+  str->type = CCL_STR;
+  str->value.as_str.size = size;
+  str->value.as_str.buffer = buffer;
+
+  return str;
+}
+
+CCL_Object *CCL_list_new(int argc, ...) {
+  CCL_Object *list = malloc(sizeof(CCL_Object));
+  int i;
+  va_list ap;
+  va_start(ap, argc);
+
+  list->type = CCL_LIST;
+  list->value.as_list.size = argc;
+  list->value.as_list.capacity = argc < CCL_MIN_LIST_SIZE ? CCL_MIN_LIST_SIZE : argc;
+  list->value.as_list.buffer = malloc(sizeof(CCL_Object*) * list->value.as_list.capacity);
+
+  for (i = 0; i < argc; i++)
+    list->value.as_list.buffer[i] = va_arg(ap ,CCL_Object*);
+
+  va_end(ap);
+  return list;
+}
+
+void CCL_list_add(CCL_Object *list, CCL_Object *item) {
+  if (list->value.as_list.size == list->value.as_list.capacity) {
+    list->value.as_list.capacity *= 2;
+    list->value.as_list.buffer = realloc(list->value.as_list.buffer, sizeof(CCL_Object*) * list->value.as_list.capacity);
+  }
+
+  list->value.as_list.buffer[list->value.as_list.size++] = item;
+}
+
+CCL_Object *CCL_list_pop(CCL_Object *list) {
+  return list->value.as_list.buffer[--list->value.as_list.size];
+}
+
+CCL_Object *CCL_dict_new(int argc, ...) {
+  CCL_Object *dict = malloc(sizeof(CCL_Object));
+  va_list ap;
+  va_start(ap, argc);
+
+  dict->type = CCL_DICT;
+  dict->value.as_dict = NULL;
+
+  assert(argc % 2 == 0);
+
+  for (; argc; argc -= 2) {
+    CCL_Object *key = va_arg(ap, CCL_Object*);
+    CCL_Object *value = va_arg(ap, CCL_Object*);
+    CCL_dict_setitem(dict, key, value);
+  }
+
+  va_end(ap);
+
+  return dict;
+}
+
+size_t CCL_dict_size(CCL_Object *dict) {
+  return dict->value.as_dict == NULL ? 0 : dict->value.as_dict->size;
+}
+
+void CCL_dict_setitem(CCL_Object *dict, CCL_Object *key, CCL_Object *value) {
+  /* TODO: Weight balance the tree on insertion */
+  int cmp, new_key = CCL_dict_getitem(dict, key) == NULL;
+  CCL_dict **nodeptr = &dict->value.as_dict;
+
+  while (*nodeptr != NULL && (cmp = CCL_cmp(key, (*nodeptr)->key)) != 0) {
+    if (new_key)
+      (*nodeptr)->size++;
+
+    nodeptr = &(*nodeptr)->children[cmp < 0 ? CCL_LEFT : CCL_RIGHT];
+  }
+
+  if (*nodeptr == NULL) {
+    (*nodeptr) = malloc(sizeof(CCL_dict));
+    (*nodeptr)->size = 1;
+    (*nodeptr)->children[0] =
+    (*nodeptr)->children[1] = NULL;
+    (*nodeptr)->key = key;
+  }
+
+  (*nodeptr)->value = value;
+}
+
+void CCL_dict_delitem(CCL_Object *dict, CCL_Object *key) {
+  assert(0); /* TODO */
+}
+
+CCL_Object *CCL_dict_getitem(CCL_Object *dict, CCL_Object *key) {
+  CCL_dict *node = dict->value.as_dict;
+
+  assert(dict->type == CCL_DICT);
+
+  while (node != NULL) {
+    int cmp = CCL_cmp(key, node->key);
 
     if (cmp == 0)
-      return 1;
+      return node->value;
 
-    *parent = **nodeptr;
-    *nodeptr = &(*parent)->children[*childtype = cmp < 0 ? CCL_LEFT : CCL_RIGHT];
+    node = node->children[cmp < 0 ? CCL_LEFT : CCL_RIGHT];
   }
 
-  return 0;
+  return NULL;
 }
 
 /** tests */
-#include <assert.h>
 
 void CCL_test() {
-  /* dict_find test */
+  /* num tests */
   {
-    CCL_dict d;
-    CCL_node **n;
-    d.root = NULL;
-    assert(dict_find(&d, NULL, &n, NULL, NULL) == 0);
+    assert(CCL_num_new(0) == CCL_num_new(0));
+    assert(CCL_num_new(0)->value.as_num == 0);
+    assert(CCL_num_new(1)->value.as_num == 1);
   }
+  /* list tests */
   {
-    CCL_dict d;
-    CCL_node **result, a, b;
+    CCL_Object *list1 = CCL_list_new(2, CCL_num_new(555), CCL_nil),
+               *list2 = CCL_list_new(2, CCL_num_new(555), CCL_nil),
+               *list3 = CCL_list_new(2, CCL_num_new(556), CCL_nil);
+    assert(list1->value.as_list.size == 2);
+    assert(CCL_cmp(list1, list2) == 0);
+    assert(CCL_cmp(list1, list3) < 0);
 
+    CCL_list_add(list1, CCL_true);
+    assert(list1->value.as_list.size == 3);
+    assert(list1->value.as_list.buffer[2] == CCL_true);
+    assert(CCL_cmp(list1->value.as_list.buffer[2], CCL_true) == 0);
+    assert(CCL_cmp(list1, list2) > 0);
+
+    assert(CCL_list_pop(list1) == CCL_true);
+    assert(CCL_cmp(list1, list2) == 0);
+  }
+  /* dict tests */
+  {
+    CCL_Object *dict = CCL_dict_new(0), *one = CCL_num_new(1), *two = CCL_num_new(2);
+
+    assert(CCL_dict_size(dict) == 0);
+    assert(CCL_dict_getitem(dict, CCL_nil) == NULL);
+
+    CCL_dict_setitem(dict, CCL_nil, CCL_nil);
+    assert(CCL_dict_size(dict) == 1);
+    assert(CCL_dict_getitem(dict, CCL_nil) == CCL_nil);
+
+    CCL_dict_setitem(dict, one, two);
+    assert(CCL_dict_size(dict) == 2);
+    assert(CCL_dict_getitem(dict, CCL_nil) == CCL_nil);
+    assert(CCL_dict_getitem(dict, one) == two);
+
+    CCL_dict_setitem(dict, CCL_nil, one);
+    assert(CCL_dict_size(dict) == 2);
+    assert(CCL_dict_getitem(dict, CCL_nil) == one);
+    assert(CCL_dict_getitem(dict, one) == two);
+
+    CCL_dict_setitem(dict, two, CCL_nil);
+    assert(CCL_dict_size(dict) == 3);
+    assert(CCL_dict_getitem(dict, CCL_nil) == one);
+    assert(CCL_dict_getitem(dict, one) == two);
+    assert(CCL_dict_getitem(dict, two) == CCL_nil);
+    assert(CCL_dict_getitem(dict, CCL_true) == NULL);
   }
 }
 
@@ -127,6 +327,7 @@ void CCL_test() {
 void CCL_test();
 
 int main(int argc, char **argv) {
+  CCL_init();
   CCL_test();
   return 0;
 }
