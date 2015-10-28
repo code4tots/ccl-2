90556e57a4bf57c6a4224317f30ddd6634eaacb0
Kyumin Kim <math4tots@gmail.com>
2015-10-28 03:06:39 -0700

    gcc

--- ccl.c
+++ ccl.c
@@ -1,3 +1,6 @@
+/*
+gcc -std=c89 -pedantic -Wall -Wmissing-braces -Wextra -Wmissing-field-initializers -Wformat=2 -Wswitch-default -Wswitch-enum -Wcast-align -Wpointer-arith -Wbad-function-cast -Wstrict-overflow=5 -Wstrict-prototypes -Winline -Wundef -Wnested-externs -Wcast-qual -Wshadow -Wunreachable-code -Wfloat-equal -Wstrict-aliasing=1 -Wredundant-decls -Wold-style-definition -Werror -ggdb3 -O0 -fno-omit-frame-pointer -fno-common -fstrict-aliasing -lm ccl.c
+*/
 /** header */
 #include <stddef.h>
 
@@ -52,7 +55,7 @@ void CCL_test() {
 }
 
 /** main */
-int main(int argc, char **argv) {
+int main() {
   CCL_inititialize();
   CCL_test();
   return 0;
