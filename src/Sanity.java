c91edbcc82d5cf051cabacf3291e6ab24f250cb8
Kyumin Kim <math4tots@gmail.com>
2015-12-02 23:16:31 -0800

    Sanity.java Lexer

--- src/Sanity.java
+++ src/Sanity.java
@@ -4,6 +4,15 @@ import java.util.Collections;
 
 public class Sanity {
 
+/// Flag indicating whether we should perform tests on startup.
+public static final boolean TEST = true;
+
+// Tests are run in static blocks so we don't need to load them here.
+public static void main(String[] args) {
+  if (TEST)
+    System.out.println("All tests pass!");
+}
+
 /// Effectively the core library.
 
 public static final TypeValue typeValue = new TypeValue("Value");
@@ -141,61 +150,6 @@ public abstract static class FunctionValue extends Value {
   }
 }
 
-/// Ast
-
-public abstract static class Ast {
-  public final Token token;
-  public Ast(Token token) { this.token = token; }
-  public abstract void evali(Context c);
-  public final void eval(Context c) {
-    evali(c);
-    c.check();
-  }
-}
-
-/// Lexer and Token
-public static final class Lexer {
-  public final String string;
-  public final String filespec;
-  public final ArrayList<Token> tokens;
-  public Lexer(String string, String filespec, int... ii) {
-    this.string = string;
-    this.filespec = filespec;
-    tokens = new ArrayList<Token>();
-    for (int i = 0; i < ii.length; i++)
-      tokens.add(new Token(this, ii[i]));
-  }
-}
-
-public static final class Token {
-  public final Lexer lexer;
-  public final int i;
-  public Token(Lexer lexer, int i) {
-    this.lexer = lexer;
-    this.i = i;
-  }
-  public String getLocationString() {
-    int a = i, b = i, c = i, lc = 1;
-    while (a > 1 && lexer.string.charAt(a-1) != '\n')
-      a--;
-    while (b < lexer.string.length() && lexer.string.charAt(b) != '\n')
-      b++;
-    for (int j = 0; j < i; j++)
-      if (lexer.string.charAt(j) == '\n')
-        lc++;
-
-    String spaces = "";
-    for (int j = 0; j < i-a; j++)
-      spaces = spaces + " ";
-
-    return
-        "*** In file '" + lexer.filespec + "' on line " + Integer.toString(lc) +
-        " ***\n" + lexer.string.substring(a, b) + "\n" +
-        spaces + "*\n";
-  }
-}
-
-
 /// Trace
 
 public abstract static class Trace {
@@ -252,6 +206,265 @@ public static final class CallTrace extends Trace {
   }
 }
 
+/// Ast
+
+public abstract static class Ast {
+  public final Token token;
+  public Ast(Token token) { this.token = token; }
+  public abstract void evali(Context c);
+  public final void eval(Context c) {
+    evali(c);
+    c.check();
+  }
+}
+
+/// Lexer and Token
+
+public static final ArrayList<String> KEYWORDS = toArrayList("def");
+public static final ArrayList<String> SYMBOLS;
+static {
+  SYMBOLS = toArrayList(
+    "(", ")", "[", "]", "{", "}", ".", ":",
+    "+", "-", "*", "/", "%");
+}
+
+public static final class Lexer {
+  public final String string;
+  public final String filespec;
+  public final ArrayList<Token> tokens;
+  public boolean done = false;
+  public Token peek = null;
+  public int pos = 0;
+  public Lexer(String string, String filespec) {
+    this.string = string;
+    this.filespec = filespec;
+    tokens = new ArrayList<Token>();
+    peek = null;
+
+    next();
+  }
+  public Token next() {
+    Token token = peek;
+    peek = extract();
+    tokens.add(peek);
+    return token;
+  }
+  private Token extract() {
+    skipSpacesAndComments();
+
+    if (!more()) {
+      done = true;
+      return makeToken(pos, "EOF");
+    }
+
+    int start = pos;
+
+    // STR
+    if (startsWith("r'", "'", "r\"", "\"")) {
+      boolean raw = false;
+      if (startsWith("r")) {
+        pos++;
+        raw = true;
+      }
+      String quote = startsWith("'''", "\"\"\"") ? slice(3) : slice(1);
+      pos += quote.length();
+      StringBuilder sb = new StringBuilder();
+      while (!startsWith(quote)) {
+        if (!more())
+          throw new RuntimeException("Finish your string literals");
+        if (!raw && startsWith("\\")) {
+          pos++;
+          if (!more())
+            throw new RuntimeException("Finish your string escapes");
+          switch (ch()) {
+          case '\\': sb.append('\\'); break;
+          default: throw new RuntimeException(
+              "Invalid string escape " + Character.toString(ch()));
+          }
+          pos++;
+        } else {
+          sb.append(ch());
+          pos++;
+        }
+      }
+      pos += quote.length();
+      return makeToken(start, "STR", sb.toString());
+    }
+
+    // NUM
+    boolean seenDot = false;
+    if (startsWith("+", "-"))
+      pos++;
+    if (startsWith(".")) {
+      seenDot = true;
+      pos++;
+    }
+    if (isdigit()) {
+      while (isdigit())
+        pos++;
+      if (!seenDot && startsWith(".")) {
+        pos++;
+        while (isdigit())
+          pos++;
+      }
+      return makeToken(start, "NUM", Double.valueOf(cut(start)));
+    } else {
+      pos = start;
+    }
+
+    // ID and KEYWORDS
+    while (isword())
+      pos++;
+    if (start < pos) {
+      String word = cut(start);
+      if (KEYWORDS.contains(word))
+        return makeToken(start, word);
+      else
+        return makeToken(start, "ID", word);
+    }
+
+    // SYMBOLS
+    String symbol = null;
+    for (int i = 0; i < SYMBOLS.size(); i++) {
+      String s = SYMBOLS.get(i);
+      if (startsWith(s) && (symbol == null || s.length() > symbol.length()))
+        symbol = s;
+    }
+    if (symbol != null) {
+      pos += symbol.length();
+      return makeToken(start, symbol);
+    }
+
+    // ERR
+    while (more() && !Character.isWhitespace(ch()))
+      pos++;
+    throw new RuntimeException("Unrecognized token " + cut(start));
+  }
+  public boolean isdigit() {
+    return more() && Character.isDigit(ch());
+  }
+  public boolean isword() {
+    return more() && (
+        Character.isAlphabetic(ch()) || ch() == '_' || isdigit());
+  }
+  private boolean more() {
+    return pos < string.length();
+  }
+  private boolean isspace() {
+    return more() && Character.isWhitespace(ch());
+  }
+  private String slice(int len) {
+    int end = pos + len < string.length() ? pos + len : string.length();
+    return string.substring(pos, end);
+  }
+  private String cut(int start) {
+    return string.substring(start, pos);
+  }
+  private char ch() {
+    return string.charAt(pos);
+  }
+  private void skipSpacesAndComments() {
+    while (true) {
+      while (isspace())
+        pos++;
+
+      if (!startsWith("#"))
+        break;
+
+      while (more() && !startsWith("\n"))
+        pos++;
+    }
+  }
+  private boolean startsWith(String... args) {
+    for (int i = 0; i < args.length; i++)
+      if (string.startsWith(args[i], pos))
+        return true;
+    return false;
+  }
+  private Token makeToken(int start, String type) {
+    return makeToken(start, type, null);
+  }
+  private Token makeToken(int start, String type, Object value) {
+    return new Token(this, start, type, value);
+  }
+}
+
+public static final class Token {
+  public final Lexer lexer;
+  public final int i;
+  public final String type;
+  public final Object value;
+  public Token(Lexer lexer, int i, String type, Object value) {
+    this.lexer = lexer;
+    this.i = i;
+    this.type = type;
+    this.value = value;
+  }
+  public String getLocationString() {
+    int a = i, b = i, c = i, lc = 1;
+    while (a > 1 && lexer.string.charAt(a-1) != '\n')
+      a--;
+    while (b < lexer.string.length() && lexer.string.charAt(b) != '\n')
+      b++;
+    for (int j = 0; j < i; j++)
+      if (lexer.string.charAt(j) == '\n')
+        lc++;
+
+    String spaces = "";
+    for (int j = 0; j < i-a; j++)
+      spaces = spaces + " ";
+
+    return
+        "*** In file '" + lexer.filespec + "' on line " + Integer.toString(lc) +
+        " ***\n" + lexer.string.substring(a, b) + "\n" +
+        spaces + "*\n";
+  }
+}
+
+// Lexer test
+static {
+  if (TEST) {
+    Lexer lexer;
+    Token tok;
+
+    lexer = new Lexer("'hi'", "<test>");
+    tok = lexer.next();
+    expect(tok.type.equals("STR"));
+    expect(tok.value.equals("hi"));
+
+    lexer = new Lexer("5.6", "<test>");
+    tok = lexer.next();
+    expect(tok.type.equals("NUM"));
+    expect(tok.value.equals(5.6));
+
+    lexer = new Lexer("hi", "<test>");
+    tok = lexer.next();
+    expect(tok.type.equals("ID"));
+    expect(tok.value.equals("hi"));
+
+    lexer = new Lexer("def", "<test>");
+    tok = lexer.next();
+    expect(tok.type.equals("def"));
+    expect(tok.value == null);
+
+    lexer = new Lexer(".", "<test>");
+    tok = lexer.next();
+    expect(tok.type.equals("."));
+    expect(tok.value == null);
+
+    lexer = new Lexer("a b # c", "<test>");
+    while (!lexer.done)
+      lexer.next();
+
+    expect(lexer.tokens.size() == 3);
+    expect(lexer.tokens.get(0).type.equals("ID"));
+    expect(lexer.tokens.get(1).type.equals("ID"));
+    expect(lexer.tokens.get(2).type.equals("EOF"));
+
+    System.out.println("Lexer tests pass");
+  }
+}
+
 /// utils
 public static ArrayList<Value> toArrayList(Value... args) {
   ArrayList<Value> al = new ArrayList<Value>();
@@ -259,9 +472,19 @@ public static ArrayList<Value> toArrayList(Value... args) {
     al.add(args[i]);
   return al;
 }
+public static ArrayList<String> toArrayList(String... args) {
+  ArrayList<String> al = new ArrayList<String>();
+  for (int i = 0; i < args.length; i++)
+    al.add(args[i]);
+  return al;
+}
 
 public static void err(Context c, String message) {
   throw new RuntimeException(message);
+}
+
+public static void expect(boolean cond) {
+  if (!cond) throw new RuntimeException();
 }
 
 }
