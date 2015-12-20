9ddd223dd3970d6c6ca2edebb8dbdcfe4fea4059
Kyumin Kim <math4tots@gmail.com>
2015-12-20 14:56:01 -0800

    java wildcard arguments comment
    http://stackoverflow.com/a/12407934

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 @echo off
 if not exist cls mkdir cls
+REM "Even though cmd.exe doesn't expand wildcards, java/javac"
+REM "seem to do it for us."
 "C:\Program Files\Java\jdk1.8.0_65\bin\javac.exe" -Xlint src\*.java -d cls
 "C:\Program Files\Java\jdk1.8.0_65\bin\java.exe" -cp cls Sanity %1 src\*.ccl
