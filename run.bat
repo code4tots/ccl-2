560f048bbae6135e6c69fc13d0b85f25f2c21910
Kyumin Kim <math4tots@gmail.com>
2015-12-27 16:59:34 -0800

    run.bat javac

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@
 if not exist cls mkdir cls
 REM "Even though cmd.exe doesn't expand wildcards, java/javac"
 REM "seem to do it for us."
-"C:\Program Files\Java\jdk1.8.0_65\bin\javac.exe" -Xlint src\*.java -Xdiags:verbose -d cls && "C:\Program Files\Java\jdk1.8.0_65\bin\java.exe" -cp cls DesktopSanity %1 src\*.ccl
+javac -Xlint src\*.java -Xdiags:verbose -d cls && java -cp cls SimpleDesktop %1
