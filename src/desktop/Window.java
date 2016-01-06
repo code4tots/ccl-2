f6d07cd009ae673becc9e35726d784f85a6be418
Kyumin Kim <math4tots@gmail.com>
2016-01-06 00:39:01 -0800

    gui WIP
    just use a bufferedimage… too sleepy…

--- src/desktop/Window.java
+++ src/desktop/Window.java
@@ -1,23 +1,32 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import javax.swing.*;
-import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+import javax.swing.JComponent;
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Window extends Val {
   public static final HashMap<String, Val> MM = new Hmb()
       .put("name", Str.from("gui#Window"))
       .put(new BuiltinFunc("gui#Window#__new__") {
         public Val calli(Val self, ArrayList<Val> args) {
-          final Window window = new Window();
-          SwingUtilities.invokeLater(new Runnable() {
+          // TODO: Clean up this dirty 'array as pointer' hack.
+          final JFrame[] frameptr = new JFrame[1];
+          final Panel[] panelptr = new Panel[1];
+          invokeAndWait(new Runnable() {
             public void run() {
-              window.frame = new JFrame("MyJFrame");
-              window.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-              window.frame.pack();
-              window.frame.setVisible(true);
+              JFrame frame = frameptr[0] = new JFrame("MyJFrame");
+              Panel panel = panelptr[0] = new Panel();
+              frame.add(panel);
+              frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+              frame.pack();
+              frame.setVisible(true);
             }
           });
-          return window;
+          return new Window(frameptr[0], panelptr[0]);
         }
       })
       .put(new BuiltinFunc("gui#Window#dim") {
@@ -32,7 +41,7 @@ public class Window extends Val {
                 .val.intValue();
             SwingUtilities.invokeLater(new Runnable() {
               public void run() {
-                window.frame.setPreferredSize(new Dimension(width, height));
+                window.panel.setPreferredSize(new Dimension(width, height));
                 window.frame.pack();
               }
             });
@@ -45,19 +54,13 @@ public class Window extends Val {
 
             // TODO: Hmm. invokeAndWait seems pretty pricey for just reading
             // the size of a frame. The documentation doesn't seem to
-            // explicitly indicate that getPreferredSize is threadsafe.
+            // explicitly indicate that getBounds is threadsafe.
             // So I'm playing it safe here. Look into this more.
-            try {
-              SwingUtilities.invokeAndWait(new Runnable() {
-                public void run() {
-                  dims[0] = window.frame.getPreferredSize();
-                }
-              });
-            } catch (InterruptedException e) {
-              throw new Err(e);
-            } catch (java.lang.reflect.InvocationTargetException e) {
-              throw new Err(e);
-            }
+            invokeAndWait(new Runnable() {
+              public void run() {
+                dims[0] = window.panel.getPreferredSize();
+              }
+            });
 
             return List.from(
                 Num.from(dims[0].width), Num.from(dims[0].height));
@@ -74,11 +77,30 @@ public class Window extends Val {
   // As you can see in gui#Window#__new__ function above, when a Window
   // object is created, we queue up a Runnable that sets this Window's JFrame.
 
-  // TODO: This of course depends on the assumption that the EDT runs
-  // Runnables in the order they are queued up. Check the documentation
-  // to verify that this is safe to assume.
-
-  private JFrame frame = null;
-  private Window() {}
+  private final JFrame frame;
+  private final Panel panel;
+  private Window(JFrame frame, Panel panel) {
+    this.frame = frame;
+    this.panel = panel;
+  }
   public HashMap<String, Val> getMeta() { return MM; }
+
+  // At first I thought about having separate a separate 'Panel' class,
+  // but why enforce abstractions here? I figure I could do that in the
+  // language itself.
+  // If performance really becomes an issue, I'll worry about it then.
+  private static final class Panel extends JComponent {
+    public static final long serialVersionUID = 42L;
+    public void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      g.setColor(Color.red);
+      g.fillOval(0, 0, 50, 50);
+    }
+  }
+
+  private static void invokeAndWait(Runnable r) {
+    try { SwingUtilities.invokeAndWait(r); }
+    catch (InterruptedException e) { throw new Err(e); }
+    catch (InvocationTargetException e) { throw new Err(e); }
+  }
 }
