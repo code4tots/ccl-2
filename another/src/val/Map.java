aebfe4ea363f945e451a5cb8d27150bf85b65196
Kyumin Kim <math4tots@gmail.com>
2016-01-13 03:55:55 -0800

    another Map

---
+++ another/src/val/Map.java
@@ -0,0 +1,21 @@
+package com.ccl.val;
+
+import com.ccl.err.Err;
+import java.util.HashMap;
+
+public final class Map extends Val {
+  public static Map from(HashMap<Val, Val> v) { return new Map(v); }
+  public static Map from(Val... kvs) {
+    if (kvs.length%2 != 0)
+      throw new Err("Map requires an even number of arguments");
+    HashMap<Val, Val> hm = new HashMap<Val, Val>();
+    for (int i = 0; i < kvs.length; i += 2)
+      hm.put(kvs[i], kvs[i+1]);
+    return new Map(hm);
+  }
+  public static final Meta META = new Meta("Map");
+  private final HashMap<Val, Val> val;
+  private Map(HashMap<Val, Val> val) { this.val = val; }
+  public Meta getMeta() { return META; }
+  public int hashCode() { return val.hashCode(); }
+}
--- another/src/val/Str.java
+++ another/src/val/Str.java
@@ -1,9 +1,10 @@
 package com.ccl.val;
 
 public final class Str extends Val {
+  public static Str from(String val) { return new Str(val); }
   public static final Meta META = new Meta("Str");
   private final String val;
-  public Str(String val) { this.val = val; }
+  private Str(String val) { this.val = val; }
   public Meta getMeta() { return META; }
   public int hashCode() { return val.hashCode(); }
 }
