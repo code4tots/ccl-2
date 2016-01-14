e6961d77c36018694db625597c2dc410de06e110
Kyumin Kim <math4tots@gmail.com>
2016-01-13 16:33:32 -0800

    Translator.java
    Arguably, doing a translation has the benefits that:
      1) Faster because there is no explicit tree-walking during runtime
      2) Easier to inspect performance since I can directly inspect what
    Java code was generated rather than figure out the code path that
    needed to be taken.

---
+++ src/translator/Translator.java
@@ -0,0 +1,38 @@
+
+// TODO: Finish this.
+public final class Translator extends AstVisitor<Void> {
+
+  private final StringBuilder sb = new StringBuilder();
+
+  // Statement only
+  public Void visitReturn(Ast.Return node) { return null; }
+  public Void visitWhile(Ast.While node) { return null; }
+  public Void visitBlock(Ast.Block node) { return null; }
+  public Void visitBreak(Ast.Break node) { return null; }
+  public Void visitContinue(Ast.Continue node) { return null; }
+
+  // Statement or Expression
+  public Void visitIf(Ast.If node) { return null; }
+
+  // Expression only
+  public Void visitInt(Ast.Int node) {
+    return null;
+  }
+  public Void visitFlt(Ast.Flt node) { return null; }
+  public Void visitStr(Ast.Str node) { return null; }
+  public Void visitName(Ast.Name node) { return null; }
+  public Void visitAssign(Ast.Assign node) { return null; }
+  public Void visitFunction(Ast.Function node) { return null; }
+  public Void visitCall(Ast.Call node) { return null; }
+  public Void visitGetMethod(Ast.GetMethod node) { return null; }
+  public Void visitGetAttribute(Ast.GetAttribute node) { return null; }
+  public Void visitSetAttribute(Ast.SetAttribute node) { return null; }
+  public Void visitIs(Ast.Is node) { return null; }
+  public Void visitIsNot(Ast.IsNot node) { return null; }
+  public Void visitNot(Ast.Not node) { return null; }
+  public Void visitAnd(Ast.And node) { return null; }
+  public Void visitOr(Ast.Or node) { return null; }
+
+  // Module
+  public Void visitModule(Ast.Module node) { return null; }
+}
