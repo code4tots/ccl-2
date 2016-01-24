adbaebbbf1478c4112f32489bb7a4c21a373102f
Kyumin Kim <math4tots@gmail.com>
2016-01-23 17:06:25 -0800

    Little more work on translator/

--- src/translator/Translator.java
+++ src/translator/Translator.java
@@ -7,26 +7,45 @@ public final class Translator extends AstVisitor<String> {
     return "\nreturn " + visit(node.val) + ";";
   }
   public String visitWhile(Ast.While node) {
-
-    return null;
+    return
+      "\nwhile (" + visit(node.cond) + ".truthy())" +
+      visit(node.body).replaceAll("\n", "\n  ");
   }
   public String visitBlock(Ast.Block node) { return null; }
   public String visitBreak(Ast.Break node) { return null; }
   public String visitContinue(Ast.Continue node) { return null; }
   public String visitExpressionStatement(Ast.ExpressionStatement node) {
-    return null;
+    return "\n" + visit(node.expr) + ";";
   }
 
   // Statement or Expression
-  public String visitIf(Ast.If node) { return null; }
+  public String visitIf(Ast.If node) {
+    String translation =
+      "\nif (" + visit(node.cond) + ")" +
+        visit(node.body).replaceAll("\n", "\n  ");
+
+    if (node.other != null)
+      translation +=
+        "\nelse" +
+        visit(node.other).replaceAll("\n", "\n  ");
+
+    return translation;
+  }
 
   // Expression only
   public String visitInt(Ast.Int node) {
-    return null;
+    // TODO: Make these ints constants constructed on startup.
+    return "Num.from(\"" + node.val.toString() + "\")";
   }
-  public String visitFlt(Ast.Flt node) { return null; }
+  public String visitFlt(Ast.Flt node) {
+    // TODO: Make these floats constants constructed on startup.
+    return "Num.from(" + node.val.toString() + ")";
+  }
   public String visitStr(Ast.Str node) { return null; }
-  public String visitName(Ast.Name node) { return null; }
+  public String visitName(Ast.Name node) {
+    // TODO: Die if name is not defined.
+    return "scope.getOrNull(" + node.name + ")";
+  }
   public String visitAssign(Ast.Assign node) { return null; }
   public String visitFunction(Ast.Function node) { return null; }
   public String visitCall(Ast.Call node) { return null; }
@@ -41,4 +60,12 @@ public final class Translator extends AstVisitor<String> {
 
   // Module
   public String visitModule(Ast.Module node) { return null; }
+
+
+  // Just sanity check things work as expected.
+  public static void main(String[] args) {
+    System.out.println(new Translator().visit(new Parser("1", "<test>").parseStatement()));
+    System.out.println(new Translator().visit(new Parser("hi", "<test>").parseStatement()));
+    System.out.println(new Translator().visit(new Parser("while (1) 2.0", "<test>").parseStatement()));
+  }
 }
