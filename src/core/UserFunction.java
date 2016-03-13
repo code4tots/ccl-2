8e571d25e9d5ef20c1e74c25ed1bf90e2ca1b656
Kyumin Kim <math4tots@gmail.com>
2016-03-12 17:39:56 -0800

    UserFunction@filespec/lineno

--- src/core/UserFunction.java
+++ src/core/UserFunction.java
@@ -2,7 +2,23 @@ package com.ccl.core;
 
 public final class UserFunction extends Function {
 
-  public static final Blob META = new Blob(Blob.META);
+  public static final Blob META = new Blob(Blob.META)
+      .setattr("lineno", new BuiltinFunction("UserFunction@lineno") {
+        @Override
+        public Value calli(Value owner, List args) {
+          ErrUtils.expectArglen(args, 0);
+          return Number.from(
+              owner.as(UserFunction.class).ast.token.getLineNumber());
+        }
+      })
+      .setattr("filespec", new BuiltinFunction("UserFunction@filespec") {
+        @Override
+        public Value calli(Value owner, List args) {
+          ErrUtils.expectArglen(args, 0);
+          return Text.from(
+              owner.as(UserFunction.class).ast.token.lexer.filespec);
+        }
+      });
 
   private final Ast.Function ast;
   private final Scope scope;
